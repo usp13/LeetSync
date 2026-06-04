0d57a30139151a8082210ec8c164a99de9f25cd0
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-04 21:28:36 +0530

    Time: 12 ms (68.96%) | Memory: 37.1 MB (38.61%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -4,33 +4,31 @@ public:
 
         int n = grid.size() ; // row
         int m = grid[0].size() ; // col
-
         int count = 0 ; 
+        
+        // Using HASH MAP - > O(n^2)
+      
+        map<vector<int> , int> mp ; 
 
-        // BRUTE FORCE - > O(n^3)
-        for( int i = 0 ; i < n ; i++){
-            for( int j = 0 ; j < n ; j++ ){
+        // Adding all the rows in MAP
+        for( int r = 0 ; r < n ; r++ ){
+            mp[grid[r]] ++ ;
+        }
 
-                bool equal = true ; 
+        for( int c = 0 ; c < n ; c++ ){
+           
+            vector<int> v ; // temporary vector
 
-                for( int r = 0 ; r < n ; r++ ){
-
-                    if( grid[i][r] != grid[r][j] ){
-                        equal = false ; 
-                        break ;
-                    }
-
-                }
-
-                
-                 if( equal ){
-                    count ++ ;
-                }
+            for( int r = 0 ; r < n ; r++ ){
+                v.push_back( grid[r][c] ) ; // Pushing Vector in 
             }
+
+            count += mp[v] ;  // If its present , it will be added to count
+
+          
         }
 
         return count ; 
-
 
         
     }
