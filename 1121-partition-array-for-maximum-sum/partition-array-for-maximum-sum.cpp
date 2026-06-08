80fabf6479feb51b675a76da5655acfab9cbcbff
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-08 20:35:47 +0530

    Time: 3 ms (85.39%) | Memory: 12.4 MB (59.82%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
 
-    // DP - Dynamic programming Approach
-
-    int dp[501] ; 
-
-    int solve( int i , vector<int> &arr , int k ){
-
-        int n = arr.size() ; 
-
-        if( i >= n ) {
-            return 0 ; 
-        }
-
-        if( dp[i] != -1 ){
-            return dp[i] ; 
-        }
-
-        int ans = 0 ;
-        int currmax = -1 ; 
-
-        for( int j = i ; j < n && j-i+1 <= k ; j++ ){
-
-            currmax = max( currmax , arr[j]) ; 
-
-            ans = max( ans , (j-i+1)*currmax + solve( j+1,  arr , k  ) ) ; 
-
-        }
-        return dp[i] = ans ; 
-         
-    }
-
+    // DP : Memoise + Bottom Up 
+ 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
 
         int n = arr.size()  ;
 
-        memset( dp , -1 , sizeof(dp) ) ; 
+        vector<int> dp( n+1 , 0) ;
 
-        return solve( 0 , arr , k ) ; 
+        for( int size = 1 ; size <= n ; size++ ) {
+
+            int currmax = -1 ;
+
+            for( int j = 1 ; j <= k && size - j >= 0 ; j++ ){
+
+                currmax = max( currmax , arr[size-j]) ; 
+            
+                dp[size] = max( dp[size] , (j*currmax) + dp[size - j] ) ;
+
+            }
+        }
+
+       
+        return dp[n] ;
+
+        
         
     }
 };
