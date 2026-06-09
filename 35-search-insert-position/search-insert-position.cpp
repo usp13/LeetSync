e0c76bbed00915f53ba234d68d3aaa9b0878c85d
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-09 19:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (41.72%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+
+//     int LOWERBOUND(vector<int>& nums, int n ,int target){
+
+//         int l = 0;
+//         int r = n -1;
+//         int answer = n ;
+
+//         while (l<=r){
+//             int mid = (l + r) /2 ;
+//             if(nums[mid]>= target){
+//                 answer = min(answer, mid ) ;
+//                 r = mid - 1;
+//             }
+//             else {
+//                 l = mid + 1 ;
+//             }
+//         }
+//     return answer ;
+//       }
+
+    int searchInsert(vector<int>& nums, int target) {
+        // int n = nums.size();
+
+        // int answerLowerbound = LOWERBOUND(nums, n , target);
+
+        // return answerLowerbound ;
+        
+
+
+
+        // BINARY SEARCH 
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1 ;
+        int answer = n;
+
+        for( int i = 0; i<=n -1 ; i++){
+
+            int mid = (l + r) /2 ;
+            if( nums[i]==target){
+                return i ;
+            }
+
+            else if( target <= nums[mid]){
+                answer = min(answer , mid) ;
+                r = mid - 1;
+
+            }
+            else {
+                l = mid + 1 ;
+            }
+
+        }
+        return answer;
+        
+    }
+};
