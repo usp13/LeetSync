4e9d92b658d24a47f455725fe096b7a0700a1e03
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-09 19:52:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (38.28%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        int n = nums.size() ; 
+
+        // Binary Search 
+        int l = 0 ; 
+        int r = n-1 ;
+
+        while( l < r){
+
+            int mid = l + ( r-l ) / 2 ; 
+
+            if( nums[mid] > nums[mid + 1]){ // Search on the left hand side
+                r = mid ;
+            }
+
+            else {
+                l = mid + 1 ; 
+            }
+
+        }
+        return l ; 
+    }
+};
