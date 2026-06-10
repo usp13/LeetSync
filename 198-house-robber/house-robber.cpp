bf8a72f0217e909b346bc5f346486cb519717b41
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-11 00:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (43.59%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+
+        if(nums.size() == 1 ) return nums[0];
+
+        vector<int> dp( n + 1);
+
+        dp[0] = nums[0]; 
+
+        dp[1] = max(nums[0], nums[1]);
+
+        for( int i = 2 ; i<nums.size(); i++){
+            dp[i] = max( dp[i-1] , dp[i-2] + nums[i]) ;
+        }
+
+        return dp[n-1];
+        
+    }
+};
