b0771e8d328bbba046b297e1331449d7083822cf
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-13 20:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.01%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+            
+        //KADANE's ALGO 
+
+        int maxsum = nums[0]  ; // initialise with first value
+        int currsum = 0 ; 
+
+
+        for( int i = 0; i < nums.size() ; i++ ){
+
+            currsum += nums[i] ;
+
+            maxsum = max( maxsum , currsum ) ;
+
+            if( currsum < 0 ){ // if it becomes negetive
+                currsum = 0 ; // reinitialise the currsum to 0 
+            }
+        }
+
+        return maxsum ; 
+        
+        
+    }
+};
