7a77b129be42da1f9f782e3442b67982e2d7da02
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-16 20:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (75.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -7,24 +7,16 @@ public:
 
         if( n != m ) return false ;
 
-        unordered_map<char,int> mp ; 
+        int freq[26] = {0};
 
-        for( int i = 0 ; i < n ; i++ ){
-            mp[s[i]] ++ ; // adding all the characters of sting s in map
+        for(char c : s) freq[c - 'a']++;
+        for(char c : t) freq[c - 'a']--;
+
+        for(int x : freq){
+            if(x != 0) return false;
         }
 
-        for( int i = 0 ; i < m ; i++ ){
-            mp[t[i]] -- ; // removing all the characters of sting s in map
-        }
-
-       for( auto &p : mp ){
-            if( p.second != 0 ){
-                return false ; 
-            }
-        }
-
-        return true ; 
-
+        return true;
 
         
     }
