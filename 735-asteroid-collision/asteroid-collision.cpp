617985323785ecb62a50b318f9aeb8be2195f262
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-30 01:31:15 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (41.45%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        // STACK : Collision in the path is affecting the elements behind/before it.
+
+        stack<int> st ; 
+
+        for( int &a : asteroids){
+            // Condition of Collision 
+            while( !st.empty() && a < 0 && st.top() > 0  ){
+
+                int sum = a + st.top() ; 
+
+                if( sum < 0 ){
+                    st.pop() ; 
+                }
+                else if( sum > 0 ){
+                    a = 0 ;
+                
+                }
+                else { // sum = 0 
+                    st.pop() ;
+                    a = 0 ; 
+                }
+            }
+
+            if( a != 0 ){
+                st.push(a) ; 
+            }
+        }
+        
+        int s = st.size() ; 
+
+        vector<int> ans(s) ;
+
+        int i = s-1 ; 
+
+        while( !st.empty() ){
+
+            ans[i] = st.top() ; 
+
+            st.pop() ; 
+
+            i -- ; 
+        } 
+
+        return ans ;
+    }
+};
