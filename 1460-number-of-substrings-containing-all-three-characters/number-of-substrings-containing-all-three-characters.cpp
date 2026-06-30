f1c990f8e7b2ee9fdbe6cff7bdb274728e86e797
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-06-30 10:59:45 +0530

    Time: 2 ms (91.13%) | Memory: 11.2 MB (21.13%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        
+        int n = s.length();
+
+        int left = 0;
+        int ans = 0;
+
+        vector<int> count(3, 0);
+
+        // SLIDING WINDOW
+        for (int right = 0; right < n; right++) {
+
+            count[s[right] - 'a']++;
+
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                ans += n - right;
+
+                count[s[left] - 'a']--;
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
