55d7c6ec9845782838fb6c5072bc86abc6038ee0
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-05 19:25:42 +0530

    Time: 5 ms (85.37%) | Memory: 11.5 MB (68.29%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
@@ -1,94 +1,74 @@
 class Solution {
 public:
-    int n;
-    const int modd = 1e9 + 7;
-    vector<vector<pair<int,int>>> dp;
 
-    int getIntFromChar(char ch){
-        return (ch == 'S' || ch == 'E') ? 0 : ch - '0';
-    }
-
-    bool isvalid(int i, int j, vector<string>& board){
-        return (i >= 0 && i < n && j >= 0 && j < n && board[i][j] != 'X');
-    }
-
-    // DP : Recursion + Memoisation ! 
-
-    pair<int,int> solve(int i, int j, vector<string>& board){
-
-        // {score, paths}
-        if(board[i][j] == 'E')
-            return {0,1};
-
-        if(board[i][j] == 'X')
-            return {0,0};
-
-        if(dp[i][j] != make_pair(-1,-1))
-            return dp[i][j];
-
-        // initialize scores as -1
-        int upscore = -1, leftscore = -1, diascore = -1;
-        int uppath = 0, leftpath = 0, diapath = 0;
-
-        char ch = board[i][j];
-
-        // Up
-        if(isvalid(i-1, j, board)){
-            auto [score, path] = solve(i-1, j, board);
-            if(path){
-                upscore = score + getIntFromChar(ch);
-                uppath = path;
-            }
-        }
-
-        // Left
-        if(isvalid(i, j-1, board)){
-            auto [score, path] = solve(i, j-1, board);
-            if(path){
-                leftscore = score + getIntFromChar(ch);
-                leftpath = path;
-            }
-        }
-
-        // Diagonal
-        if(isvalid(i-1, j-1, board)){
-            auto [score, path] = solve(i-1, j-1, board);   // FIXED
-            if(path){
-                diascore = score + getIntFromChar(ch);
-                diapath = path;
-            }
-        }
-
-        // No valid path
-        if(uppath == 0 && leftpath == 0 && diapath == 0)
-            return dp[i][j] = {0,0};
-
-        int bestscore = max({upscore, leftscore, diascore});
-        int bestpath = 0;
-
-        if(upscore == bestscore)
-            bestpath = (bestpath + uppath) % modd;
-
-        if(leftscore == bestscore)
-            bestpath = (bestpath + leftpath) % modd;
-
-        if(diascore == bestscore)
-            bestpath = (bestpath + diapath) % modd;
-
-        return dp[i][j] = {bestscore, bestpath};
-    }
-
+    // DP : BOTTOM UP Approach 
     vector<int> pathsWithMaxScore(vector<string>& board) {
 
-        n = board.size();
+        int n = board.size();
+        const int MOD = 1e9 + 7;
 
-        dp.assign(n, vector<pair<int,int>>(n, {-1,-1}));
+        // dpScore[i][j] = maximum score from E to (i,j)
+        // dpPath[i][j] = number of maximum-score paths
+        vector<vector<int>> dpScore(n, vector<int>(n, -1));
+        vector<vector<int>> dpPath(n, vector<int>(n, 0));
 
-        auto ans = solve(n-1, n-1, board);
+        dpScore[0][0] = 0;
+        dpPath[0][0] = 1;
 
-        if(ans.second == 0)
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(board[i][j] == 'X') continue;
+                if(i == 0 && j == 0) continue;
+
+                int bestScore = -1;
+                int ways = 0;
+
+                // from up
+                if(i > 0 && dpPath[i-1][j] > 0) {
+                    if(dpScore[i-1][j] > bestScore) {
+                        bestScore = dpScore[i-1][j];
+                        ways = dpPath[i-1][j];
+                    } else if(dpScore[i-1][j] == bestScore) {
+                        ways = (ways + dpPath[i-1][j]) % MOD;
+                    }
+                }
+
+                // from left
+                if(j > 0 && dpPath[i][j-1] > 0) {
+                    if(dpScore[i][j-1] > bestScore) {
+                        bestScore = dpScore[i][j-1];
+                        ways = dpPath[i][j-1];
+                    } else if(dpScore[i][j-1] == bestScore) {
+                        ways = (ways + dpPath[i][j-1]) % MOD;
+                    }
+                }
+
+                // from diagonal
+                if(i > 0 && j > 0 && dpPath[i-1][j-1] > 0) {
+                    if(dpScore[i-1][j-1] > bestScore) {
+                        bestScore = dpScore[i-1][j-1];
+                        ways = dpPath[i-1][j-1];
+                    } else if(dpScore[i-1][j-1] == bestScore) {
+                        ways = (ways + dpPath[i-1][j-1]) % MOD;
+                    }
+                }
+
+                if(ways == 0) continue;
+
+                int val = 0;
+                if(board[i][j] != 'S' && board[i][j] != 'E')
+                    val = board[i][j] - '0';
+
+                dpScore[i][j] = bestScore + val;
+                dpPath[i][j] = ways;
+            }
+        }
+
+        if(dpPath[n-1][n-1] == 0)
             return {0,0};
 
-        return {ans.first, ans.second};
+        return {dpScore[n-1][n-1], dpPath[n-1][n-1]};
     }
 };
+        
