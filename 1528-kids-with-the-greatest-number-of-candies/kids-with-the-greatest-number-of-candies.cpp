d95e829b7fe643beb5b785510c91d9d24d0c5057
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-10 00:22:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.32%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+
+        int n = candies.size() ; 
+        int maxelement = *max_element( candies.begin() , candies.end() ) ;
+
+        vector<bool> ans(n , false ) ; 
+
+        for( int i = 0 ; i < n ; i++  ){
+
+            if( candies[i] + extraCandies >= maxelement ){
+                ans[i] = true ; 
+            }
+        }
+
+        return ans ; 
+        
+    }
+};
