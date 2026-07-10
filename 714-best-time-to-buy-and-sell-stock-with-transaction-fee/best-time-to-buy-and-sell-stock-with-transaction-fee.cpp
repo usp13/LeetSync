7b7e2690d3a09dd6bf91c33f1837f0c2b6f21f00
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-11 02:01:25 +0530

    Time: 124 ms (24.72%) | Memory: 108.7 MB (8.41%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 
     // BUY function : solve(prices, day + 1, n, true);
+
     // SELL function : solve(prices, day + 1, n, false);
 
     // Transaction Fee applies on a transaction
@@ -25,17 +26,23 @@ public:
 
         if (buy) {
 
+            // Buying the stock, so money is deducted from profit.
+            // After buying today, we move to the next day with the ability to SELL.
             int take = solve(prices, day + 1, n, false) - prices[day];
 
+            // Skip buying today, remain in BUY state for the next day.
             int nottake = solve(prices, day + 1, n, true);
 
             profit = max(take, nottake);
-
         }
         else {
 
+            // Selling the stock today:
+            // Today's price is added to the profit and the transaction fee is deducted.
+            // After selling, we move to the next day with the ability to BUY again.
             int sell = prices[day] + solve(prices, day + 1, n, true) - FEE;
 
+            // Skip selling today, remain in SELL state for the next day.
             int notsell = solve(prices, day + 1, n, false);
 
             profit = max(sell, notsell);
@@ -48,11 +55,11 @@ public:
 
         n = prices.size();
 
-        FEE = fee;
+        FEE = fee; // Store transaction fee globally
 
         dp.assign(n, vector<int>(2, -1));
 
-        bool buy = true;
+        bool buy = true; // Initially, we are allowed to buy.
 
         return solve(prices, 0, n, buy);
     }
