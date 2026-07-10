dae721e5eaf1b43f01999792dd3af0fa89d101d6
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-11 02:08:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (97.54%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-
-    int dp[100001];
-
-    // DP : Recursion + Memoization
-    int solve(int num) {
-
-        if (num == 0) {
-            return 0;
-        }
-
-        if (dp[num] != -1) {
-            return dp[num];
-        }
-
-        // Remove the least significant set bit and count it.
-        return dp[num] = 1 + solve(num & (num - 1));
-    }
-
     vector<int> countBits(int n) {
 
-        memset(dp, -1, sizeof(dp));
+        vector<int> ans (n+1)  ;
 
-        vector<int> ans;
-
-        for (int i = 0; i <= n; i++) {
-            ans.push_back(solve(i));
+        if( n == 0 ){
+            return ans ; 
         }
 
-        return ans;
+        ans[0] = 0 ; 
+
+        for( int i = 0 ; i <= n ; i++ ){
+
+            if( i % 2 != 0 ){ // odd number will have 1 a 0th bit in binary 
+                ans[i] = ans[i/2] + 1 ; 
+            }
+            else {
+                ans[i] = ans[i/2] ; 
+            }
+        }
+
+        return ans ; 
+        
     }
 };
