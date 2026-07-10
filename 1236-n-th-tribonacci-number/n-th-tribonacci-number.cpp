306bf2e6ecc734fb1f6a40fb61e5e3f581aadfaf
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-11 00:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.58%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
 
-    int dp[40] ; 
-
-    int solve( int n ){
+    // int dp[40] ; 
+    // DYnamic Programming : Recursion + Memoization
+    // int solve( int n ){
         
 
-        if( n == 0 ) return 0 ; 
-        if( n == 1 || n == 2 ) return 1 ; 
+    //     if( n == 0 ) return 0 ; 
+    //     if( n == 1 || n == 2 ) return 1 ; 
 
-        if( dp[n] != -1 ){
-            return dp[n] ; 
-        }
+    //     if( dp[n] != -1 ){
+    //         return dp[n] ; 
+    //     }
 
-        int f1 = solve(n-1) ; 
-        int f2 = solve(n-2) ; 
-        int f3 = solve(n-3) ; 
+    //     int f1 = solve(n-1) ; 
+    //     int f2 = solve(n-2) ; 
+    //     int f3 = solve(n-3) ; 
 
-        return dp[n] = ( f1 + f2 + f3 ) ; 
+    //     return dp[n] = ( f1 + f2 + f3 ) ; 
 
-    }
+    // }
 
     int tribonacci(int n) {
 
-        memset( dp , -1 , sizeof(dp)) ; 
+        // memset( dp , -1 , sizeof(dp)) ; 
 
-        return solve(n) ; 
+        // return solve(n) ; 
+
+        if( n == 0 ){
+            return 0 ;
+        }
+        if( n == 1 || n == 2 ){
+            return 1 ;
+        }
+        // Constant Space ! 
+        int a = 0 ; 
+        int b = 1 ; 
+        int c = 1 ;
+        int d = a + b + c ;
+
+        for( int i = 3 ; i <= n ; i++ ){
+
+            d = a + b + c ; 
+            a = b ; 
+            b = c ; 
+            c = d ;
+            
+        }
         
+        return d ; 
+
     }
 };
