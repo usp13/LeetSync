bc8aa1498c25671665a3f61f840b60036b61a328
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-13 23:59:28 +0530

    Time: 3 ms (6.13%) | Memory: 16.8 MB (5.30%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void inorder(TreeNode* root , string &s  ){
+
+        if( root == NULL ) return ; 
+
+        if( root->left == NULL && root->right == NULL ){
+            s += to_string(root->val) + "_" ; 
+            return ; 
+        }
+        
+        inorder( root->left , s ) ;
+        inorder( root->right , s ) ;
+
+    }
+
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+
+        // Traverse to all the tree's leaf and compare 
+
+        string s1 = "" ; 
+        string s2 = "" ; 
+
+        inorder( root1 , s1 ) ;
+        inorder( root2 , s2 ) ;
+
+        if( s1 == s2 ) return true ;
+
+        return false ; 
+        
+    }
+};
