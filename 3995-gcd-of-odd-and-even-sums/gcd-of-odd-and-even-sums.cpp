7754550bbdb67d508c815441af37b35f3324321e
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-15 11:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.02%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+
+        int odd = n * n ; 
+        
+        int even = n * (n-1) ; 
+
+        return gcd( odd , even ) ; 
+
+        
+    }
+};
