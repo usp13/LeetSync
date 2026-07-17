6b25d2fb6b2a5e711685693d89280424af8f73f8
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-18 00:23:57 +0530

    Time: 7 ms (24.84%) | Memory: 10.7 MB (98.93%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        
+        string ans = "" ; 
+
+        int n = num.length() ; 
+       
+
+        for( int i = 0 ; i < n ; i++ ){
+
+            while( ans.length() > 0 && ans.back() > num[i] && k > 0 ){
+
+                ans.pop_back() ; // just like pop operation in stack
+                k-- ;  
+            }
+
+            if( ans.length() > 0 || num[i] != '0' ){
+                ans.push_back(num[i]) ; 
+            }
+        }
+
+       while (k > 0 && !ans.empty()) {
+            ans.pop_back();
+            k--;
+        }
+
+        if( ans == "") return "0" ; 
+
+
+        return ans ; 
+    }
+};
