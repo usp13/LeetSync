23f198de1b3e9aebc998208c22a1e823f615c836
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-19 11:08:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (68.83%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+        vector<int> last(26);
+        vector<bool> visited(26);
+
+        string ans = "";
+
+        int n = s.length();
+
+        for( int i = 0 ; i < n ; i++){
+            last[s[i] - 'a'] = i;
+        }
+
+        for( int i = 0 ;  i< n ; i++){
+            if( visited[s[i] - 'a'])
+            continue;
+
+
+            while(!ans.empty() && ans.back() > s[i] && last[ans.back() -'a']> i){
+                visited[ans.back() - 'a'] = false ;
+                ans.pop_back();
+            }
+
+            ans.push_back(s[i]) ;
+            visited[s[i] - 'a'] = true ;
+        }
+
+        return ans ;
+        
+    }
+};
