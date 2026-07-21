ef5ef42bff424b438e5c3860d60d5c855abc4191
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-21 13:44:19 +0530

    Time: 103 ms (29.28%) | Memory: 79.1 MB (46.85%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int ones = 0; 
+        for( int i = 0 ; i < s.length() ; i++ ){
+            if(s[i] == '1'){
+                ones ++ ; 
+            }
+        }
+
+        s = "1" + s + "1" ; 
+
+        int n = s.length() ; 
+        int i = 0 ;
+
+        int ans = ones ; 
+
+        while( i < n && s[i] == '1' ){
+            i++ ; 
+        } 
+
+        int c10 = 0 ; 
+        while( i < n && s[i] == '0' ){
+            c10++ ; 
+            i++ ; 
+        }
+
+        while( i < n ){
+            
+            int c11 = 0 ; 
+            while( i < n && s[i] == '1' ){
+                c11++ ; 
+                i++ ; 
+            }
+        
+            if( c11 == 0 ){
+                break; 
+            }
+
+            int c20 = 0 ; 
+            while( i < n && s[i] == '0' ){
+                c20++ ; 
+                i++ ; 
+            }
+
+            if( c20 == 0 ){
+                break ; 
+            }
+
+            ans = max( ans , ones + c10 + c20 ) ; 
+
+            c10 = c20 ; 
+            
+
+        
+        }
+
+        return ans ; 
+        
+    }
+};
