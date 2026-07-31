0bebd38a22188e04509595cef34fcd3f261d0f14
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-07-31 11:32:10 +0530

    Time: 4 ms (86.59%) | Memory: 27 MB (46.22%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -6,8 +6,8 @@ public:
 
         int maxFreq = 0;
 
-        // Count frequencies
-        for(char ch : word) {
+        // Count the frequency of every letter
+        for (char ch : word) {
             freq[ch - 'a']++;
             maxFreq = max(maxFreq, freq[ch - 'a']);
         }
@@ -15,21 +15,22 @@ public:
         // Bucket sort frequencies
         vector<int> bucket(maxFreq + 1, 0);
 
-        for(int f : freq) {
-            if(f > 0) {
+        for (int f : freq) {
+            if (f > 0) {
                 bucket[f]++;
             }
         }
 
         int ans = 0;
-        int rank = 0; // position among used letters
+        int rank = 0;   // Position among the used letters
 
-        // Process frequencies from largest to smallest
-        for(int f = maxFreq; f >= 1; f--) {
+        // Process frequencies from highest to lowest
+        for (int f = maxFreq; f >= 1; f--) {
 
-            while(bucket[f]--) {
+            while (bucket[f]--) {
 
-                int press = rank / 8 + 1;
+                // Every group of 8 letters requires one extra key press
+                int press = (rank / 8) + 1;
 
                 ans += press * f;
 
