2184b1dbe1ef7a54e86313dc2eea35d8e92c0f7a
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-08-20 10:04:24 +0530

    Time: 3 ms (23.77%) | Memory: 23.7 MB (83.63%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+
+        int n = nums.size() ; 
+        vector<int> arr1 ; 
+        vector<int> arr2 ;
+
+        arr1 = { nums[0]} ; 
+        arr2 = { nums[1]} ;
+
+        for( int i = 2 ; i < n ; i++ ){
+
+            if( arr1.back() > arr2.back() ){
+                arr1.push_back( nums[i] ) ; 
+            }
+            else {
+                arr2.push_back( nums[i] ) ; 
+            }
+            
+        }  
+
+        for( auto val : arr2 ){
+            arr1.push_back(val) ; // append arr1 with arr2
+        }
+
+        return arr1 ; 
+        
+    }
+};
