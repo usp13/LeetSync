ab42f616e5dbaf701fdddc24955adc27568f7f58
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-09-17 19:33:26 +0530

    Time: 411 ms (62.98%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 
-SELECT e1.name as Employee
+SELECT e1.name AS Employee
 FROM Employee e1 
-JOIN Employee e2 
+LEFT JOIN Employee e2 
 ON e1.managerId = e2.id 
 WHERE e1.salary > e2.salary
 
