4f138d8c4fb22f3130badb305a1856c796e9d590
Utsav Shailesh Panchal <utsav.p@ahduni.edu.in>
2026-09-17 20:09:56 +0530

    Time: 1073 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT id
+FROM Weather w1
+WHERE temperature > (
+    SELECT temperature
+    FROM Weather w2
+    WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+);
